d352ec83d66023f85fa4375d246fc24cc900f488
signalwerk <sh@signalwerk.ch>
2019-12-23 01:52:39 +0100

    MOD: adjust params

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 # build a stand-alone program and run
 
-
 clang++ ./src/main.cpp -std=c++11 -stdlib=libc++ -o ./build/mini-browser
 
-# JSON5 (https://json5.org/) output:
-./build/mini-browser
+# json
+./build/mini-browser test/basic/index.html test/basic/index.json
